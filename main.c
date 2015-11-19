1fb191c4f420eab7f5011129a5b607832be1cd53
mkhannouz <martin.khannouz@enseirb-matmeca.fr>
2015-11-19 11:25:27 +0100

    Few change.

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define FOSC 16000000// Clock Speed
-#define BAUD 300 
+#define FOSC 16000000			// Clock Speed
+#define BAUD 300
 #define MYUBRR FOSC/16/BAUD-1
-#define FIFO_SIZE 1024 //2K bytes de RAM
+#define FIFO_SIZE 1024			//2K bytes de RAM
 #define DATA PB0
 #define CLOCK PD2
-unsigned char fifo_to_send[FIFO_SIZE]; 
+unsigned char fifo_to_send[FIFO_SIZE];
 unsigned int head_fifo = 0, back_fifo = 0;
 bool is_capture_on = false;
 unsigned char accumulateur = 0, counter = 0;
@@ -31,34 +31,35 @@ void USART_Init(unsigned int ubrr)
 
 bool USART_Transmit(unsigned char data)
 {
-	unsigned int tmp = (back_fifo+1)%FIFO_SIZE;
+	unsigned int tmp = (back_fifo + 1) % FIFO_SIZE;
 	if(tmp == head_fifo)
 		return false;
 	fifo_to_send[back_fifo] = data;
 	back_fifo = tmp;
-	UCSR0B |= _BV(UDRIE0); //Réactive les interruptions pour l'envois
+	UCSR0B |= _BV(UDRIE0);		//Réactive les interruptions pour l'envois
 	return true;
 }
+
 ISR(USART_UDRE_vect)
 {
 	UDR0 = fifo_to_send[head_fifo];
-	head_fifo = (head_fifo+1)%FIFO_SIZE;
-	if(head_fifo == back_fifo) //Disable interupt when fifo is empty
+	head_fifo = (head_fifo + 1) % FIFO_SIZE;
+	if(head_fifo == back_fifo)	//Disable interupt when fifo is empty
 		UCSR0B &= ~(1 << UDRIE0);
 }
+
 ISR(PCINT0_vect)
 {
 	USART_Transmit(PINB);
 }
+
 int main()
 {
 	USART_Init(MYUBRR);
-
-	PCICR |= _BV(PCIE0); //Enable interrupt on PORTB
-	PCMSK0 = ~0; //Enable on whole PORTB and not a specifique pin
+	PCICR |= _BV(PCIE0);		//Enable interrupt on PORTB
+	PCMSK0 = ~0;
 
 	//Enable intterrupt
 	SREG = 0x80;
 	while(1);
 }
-
